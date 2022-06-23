befccec08d2f4e664f9ee0f396d212aca90a7f1c
Victor Mota <68482280+jasonluizwf@users.noreply.github.com>
2022-06-22 21:57:57 -0300

    Criando projeto

---
+++ src/main/java/br/aprendizado/projetotest/ProjetoTestApplication.java
@@ -0,0 +1,13 @@
+package br.aprendizado.projetotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTestApplication.class, args);
+	}
+
+}
